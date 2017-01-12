807642a9a9337bbd998827f07cbadeb31444ed03
Eric Bailey <eric@ericb.me>
2017-01-12 01:24:06 -0600

    cabal2nix --shell . >shell.nix

---
+++ festhest/shell.nix
@@ -0,0 +1,28 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, stdenv }:
+      mkDerivation {
+        pname = "festhest";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [ base ];
+        homepage = "https://github.com/yurrriq/festhest";
+        description = "Example app using nixops";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
