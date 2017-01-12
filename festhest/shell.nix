f4b4db20e0d9785d8df24fa1f622c158b15fd61e
Eric Bailey <eric@ericb.me>
2017-01-12 01:30:22 -0600

    Add yesod dependency

--- festhest/shell.nix
+++ festhest/shell.nix
@@ -4,14 +4,14 @@ let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, base, stdenv }:
+  f = { mkDerivation, base, stdenv, yesod }:
       mkDerivation {
         pname = "festhest";
         version = "0.1.0.0";
         src = ./.;
         isLibrary = false;
         isExecutable = true;
-        executableHaskellDepends = [ base ];
+        executableHaskellDepends = [ base yesod ];
         homepage = "https://github.com/yurrriq/festhest";
         description = "Example app using nixops";
         license = stdenv.lib.licenses.bsd3;
